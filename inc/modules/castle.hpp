c9f00dd6c3c08b2129a8f06e7b8e61d426f03ab5
Dave M <davemoore22@gmail.com>
2025-11-01 21:27:51 +0000

    Unignoring Modules Header Folder

---
+++ inc/modules/castle.hpp
@@ -0,0 +1,69 @@
+// Copyright (C) 2025 Dave Moore
+//
+// This file is part of Sorcery.
+//
+// Sorcery is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 2 of the License, or (at your option) any later
+// version.
+//
+// Sorcery is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Sorcery.  If not, see <http://www.gnu.org/licenses/>.
+//
+// If you modify this program, or any covered work, by linking or combining
+// it with the libraries referred to in README (or a modified version of
+// said libraries), containing parts covered by the terms of said libraries,
+// the licensors of this program grant you additional permission to convey
+// the resulting work.
+
+#pragma once
+
+#include "common/define.hpp"
+#include "common/include.hpp"
+#include "core/include.hpp"
+#include "modules/inn.hpp"
+#include "modules/shop.hpp"
+#include "modules/tavern.hpp"
+#include "modules/temple.hpp"
+#include "types/enum.hpp"
+
+namespace Sorcery {
+
+// Forward Declarations
+class Controller;
+class Display;
+class Game;
+class System;
+class UI;
+
+class Castle {
+
+	public:
+		// Standard Constructor
+		Castle(System *system, Display *display, UI *ui,
+			   Controller *controller);
+
+		// Public Methods
+		auto start(Game *game) -> int;
+		auto stop() -> int;
+
+	private:
+		// Private Methods
+		auto _initialise() -> bool;
+
+		// Private Members
+		System *_system;
+		Display *_display;
+		UI *_ui;
+		Controller *_controller;
+		std::unique_ptr<Inn> _inn;
+		std::unique_ptr<Shop> _shop;
+		std::unique_ptr<Tavern> _tavern;
+		std::unique_ptr<Temple> _temple;
+};
+
+};
